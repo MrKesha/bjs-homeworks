f91c7bae0ce49211b17ec04bba902197a81be585
MrKesha <dik_nikita@mail.ru>
2020-10-08 00:30:17 +0300

    task completed

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,30 @@
+"use strict";
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    percent = Number(percent);
+    contribution = Number(contribution);
+    amount = Number(amount);
+
+    let refundAmount = amount - contribution; // тело кредита, то что надо вернуть
+    let months; // количество выплачиваемых месяцев
+    months = (date.getFullYear() - new Date().getFullYear()) * 12;
+    // months -= new Date().getFullYear() + 1;
+    // months += date.getFullYear(); 
+    let p = percent/100/12; // 1/12 процентной ставки
+    let monthlyFee = refundAmount * (p+p/((Math.pow(1+p, months)-1))); // ежемесячный платеж.
+    let totalAmount =months * monthlyFee;
+
+    console.log(totalAmount.toFixed(2));
+    return Number(totalAmount.toFixed(2));
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    let greeting;
+    if (name == undefined || name == null || name == "") {
+        greeting = "Привет, мир! Меня зовут Аноним";
+    } else if (name != null) {
+        greeting = `Привет, мир! Меня зовут ${name}`
+    }
+    console.log(greeting);
+    return greeting;
 }
