1ab29b5fb8c42ee10de1e5fcd9a731fe992cc236
MrKesha <dik_nikita@mail.ru>
2020-10-30 20:49:58 +0300

    task completed#1

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,26 @@
-//String.prototype.isPalindrome - для задачи №1
-
-function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+String.prototype.isPalindrome = function(source) {
+  const string = this.toLowerCase().replace(/\s/g, '').split('');
+  return string.join('') === string.reverse().join('');
 }
 
+
+function getAverageMark(marks) {
+  let sum = marks.reduce((partial_sum, a) => partial_sum + a,0);
+  if (sum <= 0) {
+    return 0;
+  } else if (sum > 0) {
+    let averagee = sum / marks.length;
+    let roundedAverage = Math.round(averagee);
+    return roundedAverage
+  }
+}
+
+
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+  let now = Date.now();
+  birthday = new Date(birthday)
+  let diff = now - birthday;
+  let age = diff/(365*24*60*60*1000); 
+  let result = age > 18
+  return result
 }
